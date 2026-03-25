6573362e471698e43f2aa650dec47cdb694a3fe6
AnaysMR <manobazete21@gmail.com>
2026-03-25 00:19:16 -0300

    f

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.lucks.homecode',
   appName: 'HomeCode',
-  webDir: 'web/out'
+  webDir: 'web/out',
+  server: {
+    cleartext: true,
+    allowNavigation: ['174.138.65.46'],
+  },
 };
 
 export default config;
